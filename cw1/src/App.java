fd0296feda48b4ac2398c4812567694a2d551a01
Karol <kkarol.calka@gmail.com>
2022-03-19 20:38:25 +0100

    completed homework

--- cw1/src/App.java
+++ cw1/src/App.java
@@ -1,5 +1,59 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // exercise6();
+        exercise7();
+    }
+
+    static public void exercise6() {
+        System.out.println("zadanie6");
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 900) + 100;
+        }
+        System.out.print("Generated array:[ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println("] ");
+
+        System.out.print("Sorted array:[ ");
+        for (int i = 0; i < arr.length; i++) {
+            Arrays.sort(arr);
+            System.out.print(arr[i] + ",");
+        }
+        System.out.print("] ");
+    }
+
+    static public void exercise7() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input odd number larger than 5: ");
+        int n = scan.nextInt();
+        if (n < 5) {
+            while (n < 5 || n % 2 != 0) {
+                System.out.print("Input odd number larger than 5: ");
+                n = scan.nextInt();
+            }
+        } else if (n % 2 == 0) {
+            while (n % 2 == 0) {
+                System.out.print("Input odd number larger than 5: ");
+                n = scan.nextInt();
+            }
+        }
+
+        scan.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n
+                        || i == j)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
